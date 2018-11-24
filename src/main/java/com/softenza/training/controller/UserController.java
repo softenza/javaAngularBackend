43584394d363e1762bafdbb675359d92f33395f2
ygbekou <ericgbekou@hotmail.com>
2018-11-24 05:00:54 -0600

    Adding Generic Dao

--- src/main/java/com/softenza/training/controller/UserController.java
+++ src/main/java/com/softenza/training/controller/UserController.java
@@ -1,30 +1,41 @@
 package com.softenza.training.controller;
 
-import java.security.Principal;
-import java.util.Base64;
-import javax.servlet.http.HttpServletRequest; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.softenza.finentouch.model.User;
+import com.softenza.training.model.BaseEntity;
+import com.softenza.training.model.User;
+import com.softenza.training.service.GenericService;
+
 
 @RestController
+@RequestMapping(value="/service/user")
 @CrossOrigin
 public class UserController {
  
+	@Autowired 
+	@Qualifier("userService")
+	GenericService genericService;
+	
     @RequestMapping("/login")
     public boolean login(@RequestBody User user) {
         return
           user.getEmail().equals("user") && user.getPassword().equals("password");
     }
      
-    @RequestMapping("/user")
-    public Principal user(HttpServletRequest request) {
-        String authToken = request.getHeader("Authorization")
-          .substring("Basic".length()).trim();
-        return () ->  new String(Base64.getDecoder()
-          .decode(authToken)).split(":")[0];
-    }
+    
+    @RequestMapping(value="/register",method = RequestMethod.POST)
+    public BaseEntity register(@RequestBody User user) {
+	
+		BaseEntity savedUser = this.genericService.save(user);
+		
+		return savedUser;
+	}
+    
+  
 }
